38b0e1e35bcc5241117d2d555e289e1e6a6f002f
jon <kawadu1221@gmail.com>
2019-06-12 02:12:02 +0900

    Add hotpaper struct

--- gourmet.go
+++ gourmet.go
@@ -1,12 +1,47 @@
 package hotpepper
 
 import (
+	"encoding/xml"
 	"fmt"
-
-	"github.com/dghubble/sling"
+	"io/ioutil"
+	"net/http"
 )
 
-func (cli *Client) GourmetSearch() {
-	sling.New().Get(cli.host)
-	fmt.Println("a")
+type Hotpaper struct {
+	Api_version       string `xml:"api_version"`
+	Results_available string `xml:"results_available"`
+	Results_returned  string `xml:"results_returned"`
+	Results_start     string `xml:"results_start"`
+	Shops             []Shop `xml:"shop"`
+}
+type Shop struct {
+	Id           string `xml:"id"`
+	Name         string `xml:"name"`
+	Logo_image   string `xml: "logo_image"`
+	Name_kana    string `xml: "name_kana"`
+	Address      string `xml: "address"`
+	Station_name string `xml: "station_name"`
+	Ktai_coupon  string `xml: "ktai_conpon"`
+}
+
+func (cli *Client) GourmetSearch() error {
+	req, err := http.NewRequest("GET", cli.baseURL, nil)
+	if err != nil {
+		return err
+	}
+	q := req.URL.Query()
+	fmt.Println(cli.token)
+	q.Add("key", cli.token)
+	q.Add("large_area", "Z011")
+	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL.String())
+	a, _ := http.Get(req.URL.String())
+	defer a.Body.Close()
+	fmt.Println(a.Body)
+	byteArray, _ := ioutil.ReadAll(a.Body)
+	//fmt.Println(string(byteArray))
+	data := new(Hotpaper)
+	err = xml.Unmarshal(byteArray, data)
+	fmt.Println(data.Shops)
+	return nil
 }
