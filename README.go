bbebef8b6dc43c765342c882cfde51450fbb964a
jon <kawadu1221@gmail.com>
2019-07-01 00:21:07 +0900

    Fix missed filr extension

--- README.go
+++
@@ -1,1 +0,0 @@
-[![GoDoc](https://godoc.org/github.com/jon20/hotpepper-go?status.svg)](https://godoc.org/github.com/jon20/hotpepper-go)
---
+++ README.md
@@ -0,0 +1,1 @@
+[![](https://godoc.org/github.com/jon20/hotpepper-go?status.svg)](https://godoc.org/github.com/jon20/hotpepper-go)
