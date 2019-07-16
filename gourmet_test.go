e1893ecba229abc82d411ccd1afbb474e21c5f5e
jon <kawadu1221@gmail.com>
2019-07-17 00:54:56 +0900

    Add Error test

--- gourmet_test.go
+++ gourmet_test.go
@@ -2,11 +2,25 @@ package hotpepper
 
 import (
 	"os"
+	"strconv"
 	"testing"
 )
 
+func TestGourmetSearchError(t *testing.T) {
+	api := New(os.Getenv("HOTPAPER_TOKEN"))
+	_, err := api.GourmetSearch(WithAddress("東京"))
+	if err != nil {
+		t.Fatalf("Expected Error, got %v", err)
+	}
+}
 func TestGourmetSearch(t *testing.T) {
 	api := New(os.Getenv("HOTPAPER_TOKEN"))
-	api.GourmetSearch(WithID(2), WithAddress("sample"), WithLat(0.1))
-	t.Fatal("aa")
+	hotpaper, err := api.GourmetSearch(WithAddress("東京"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	totalHit, _ := strconv.Atoi(hotpaper.Results_available)
+	if totalHit <= 0 {
+		t.Fatalf("Expected hit more than 100")
+	}
 }
