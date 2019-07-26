0097c2b2af39b3a15f45b8442a98dee8830f6e6f
jon <kawadu1221@gmail.com>
2019-07-26 15:11:10 +0900

    Add ShopSearchShop struct

--- shop.go
+++ shop.go
@@ -8,3 +8,20 @@ type ShopSearch struct {
 	Count   int64  `xml: "count"`
 	Format  string `xml: "format"`
 }
+
+type ShopName struct {
+	Results           string `xml:"results"`
+	Api_version       string `xml:"api_version"`
+	Results_available string `xml:"results_available"`
+	Results_returned  string `xml:"results_returned"`
+	Results_start     string `xml:"results_start"`
+	Shops             []Shop `xml:"Shop"`
+}
+
+type ShopSearchShop struct {
+	Id        string `xml:"id"`
+	Name      string `xml:"name"`
+	Name_kana string `xml:"name_kana"`
+	Address   string `xml:"address"`
+	Desc      int64  `xml:"desc"`
+}
